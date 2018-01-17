c87ada13a28bfaa83557a38b736b82fc1f7a5d15
Miguel Fonseca <miguelcsf@gmail.com>
2018-01-17 17:32:09 +0000

    Use withState & compose, drop class component

--- editor/hooks/copy-content/index.js
+++ editor/hooks/copy-content/index.js
@@ -1,51 +1,37 @@
 /**
  * WordPress dependencies
  */
-import { ClipboardButton } from '@wordpress/components';
-import { Component } from '@wordpress/element';
+import { ClipboardButton, withState } from '@wordpress/components';
+import { compose } from '@wordpress/element';
 import { query } from '@wordpress/data';
 import { addFilter } from '@wordpress/hooks';
 import { __ } from '@wordpress/i18n';
 
-class CopyContentButton extends Component {
-	constructor() {
-		super( ...arguments );
-		this.state = { hasCopied: false };
-		this.onCopy = this.onCopy.bind( this );
-		this.onFinishCopy = this.onFinishCopy.bind( this );
-	}
-	onCopy() {
-		this.setState( { hasCopied: true } );
-	}
-	onFinishCopy() {
-		this.setState( { hasCopied: false } );
-	}
-	render() {
-		return (
-			<ClipboardButton
-				text={ this.props.editedPostContent }
-				className="components-menu-items__button"
-				onCopy={ this.onCopy }
-				onFinishCopy={ this.onFinishCopy }
-			>
-				{ this.state.hasCopied ?
-					__( 'Copied!' ) :
-					__( 'Copy All Content' ) }
-			</ClipboardButton>
-		);
-	}
+function CopyContentButton( { editedPostContent, hasCopied, setState } ) {
+	return (
+		<ClipboardButton
+			text={ editedPostContent }
+			className="components-menu-items__button"
+			onCopy={ () => setState( { hasCopied: true } ) }
+			onFinishCopy={ () => setState( { hasCopied: false } ) }
+		>
+			{ hasCopied ?
+				__( 'Copied!' ) :
+				__( 'Copy All Content' ) }
+		</ClipboardButton>
+	);
 }
 
-const Enhanced = query( ( select ) => ( {
-	editedPostContent: select( 'core/editor', 'getEditedPostContent' ),
-} ) )( CopyContentButton );
+const Enhanced = compose(
+	query( ( select ) => ( {
+		editedPostContent: select( 'core/editor', 'getEditedPostContent' ),
+	} ) ),
+	withState( { hasCopied: false } )
+)( CopyContentButton );
 
-function withCopyContentButton( children ) {
-	return [
-		...children,
-		<Enhanced key="copy-content-button" />,
-	];
-}
-
-addFilter( 'editor.EditorActions.children',
-	'core/copy-content/button', withCopyContentButton );
+addFilter(
+	'editor.EditorActions.children',
+	'core/copy-content/button',
+	( children ) =>
+		[ ...children, <Enhanced key="copy-content-button" /> ]
+);
